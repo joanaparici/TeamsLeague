92853016122d0dc642ae509a9665cf106d4b3eb6
MORANT Joan <joan.aparici@ext.soprasteria.com>
2024-05-13 09:20:05 +0000

    Feature/crud implementation

---
+++ src/main/java/com/padelteams/mixleague/controller/model/team/TeamListDTO.java
@@ -0,0 +1,13 @@
+package com.padelteams.mixleague.controller.model.team;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TeamListDTO {
+    private int id;
+    private String nombre;
+}
